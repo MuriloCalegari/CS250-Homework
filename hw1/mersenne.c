638987809f0d706b8147e7ab26f6aa1ae0f10cd3
Murilo Calegari <murilo.calegari@duke.edu>
2022-01-16 15:56:19 -0500

    First files

---
+++ hw1/mersenne.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const* argv[]) {
+    int n = atoi(argv[1]);
+
+    int mersenne = 1;
+
+    for (int i = 1; i <= n; i++) {
+        mersenne = 2 * mersenne;
+    }
+
+    mersenne--;
+
+    printf("%d", mersenne);
+
+    return mersenne;
+}
---
+++ hw1/recurse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int computefn(int n);
+
+int main(int argc, char const* argv[]) {
+    int n = atoi(argv[1]);
+
+    printf("%d", computefn(n));
+}
+
+int computefn (int n) {
+    if(n == 0) return -2;
+
+    return 3 * n + 2 * computefn(n - 1) - 2;
+}
